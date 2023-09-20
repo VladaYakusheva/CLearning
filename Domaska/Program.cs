bb7dcf16c4357da76636957c8d584d1a12ec8c3d
Владислава Якушева <muzisnvlada@gmail.com>
2023-09-20 16:02:48 +0300

    first commit

---
+++ Domaska/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int num = int.Parse(Console.ReadLine());
+int baseNum = num;
+
+int count = 0;
+bool pow = false;
+
+while (num % 3 == 0)
+{
+    num /= 3;
+    count++;
+
+    if (num == 1)
+    {
+        pow = true;
+        break;
+    }
+}
+
+if (pow == true)
+{
+    Console.WriteLine($"число {Math.Pow(3, count)} является 3 в степени {count}");
+}
+else
+{
+    Console.WriteLine($"число {baseNum} НЕ является степенью числа 3");
+}
